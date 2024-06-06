a752c5791b04a08ed7f951741e4562adc3d864c7
LucasNotaro <lucas.sn@live.com>
2024-06-06 19:01:17 -0300

    quem somos

--- src/app/quem-somos/quem-somos.component.ts
+++ src/app/quem-somos/quem-somos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-quem-somos',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './quem-somos.component.html',
   styleUrl: './quem-somos.component.css'
 })
