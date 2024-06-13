b9bd84f233a9ed4e7e02f00f16f1fda5e010b568
LucasNotaro <lucas.sn@live.com>
2024-06-12 21:18:43 -0300

    adicionado página de execução de cursos e correção de css

--- src/app/curso-desenvolvimento-web/curso-desenvolvimento-web.component.ts
+++ src/app/curso-desenvolvimento-web/curso-desenvolvimento-web.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-curso-desenvolvimento-web',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './curso-desenvolvimento-web.component.html',
   styleUrl: './curso-desenvolvimento-web.component.css'
 })
